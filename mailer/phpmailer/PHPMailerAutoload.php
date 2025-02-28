240ea81389fb847ea5c6251f62208341bdabecc3
Alisher Turubekov <develepor@mail.ru>
2025-02-28 15:48:54 +0500

    update php mailer

--- mailer/phpmailer/PHPMailerAutoload.php
+++ mailer/phpmailer/PHPMailerAutoload.php
@@ -42,8 +42,11 @@ if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
      * Fall back to traditional autoload for old PHP versions
      * @param string $classname The name of the class to load
      */
-    function __autoload($classname)
-    {
-        PHPMailerAutoload($classname);
-    }
+    spl_autoload_register(function ($class) {
+        // Пути к PHPMailer
+        $file = __DIR__ . '/' . $class . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+        }
+    });
 }
